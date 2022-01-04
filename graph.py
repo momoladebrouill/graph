8e4c91ad766b9dc318d7f8581bed3fa75059675b
momoladebrouill <momoladebrouill@gmail.com>
2022-01-04 15:54:38 +0100

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-import random
+import random,tkinter
 '''class LL():
     def __init__(self):
         self.ls=list()
@@ -12,9 +12,11 @@ import random
     __next__=lambda self:self.ls.__next__()
     __getitem__=lambda self,v:self.ls.__getitem__(v)
 '''
+
 class S():
     def __init__(this,name:str):
         this.name=name;
+        this.x,this.y=random.randrange(0,500),random.randrange(0,500)
         this.ng=list();
         this.entier=True;
         
@@ -45,8 +47,18 @@ class S():
 
 walah=list()
 word=lambda : ''.join(chr(random.randrange(65,90)) for i in range(10))
-for i in range(20):
+for i in range(5):
     walah.append(S(word()))
 for elem in walah:
     for i in range(1,3):
         random.choice(walah).add(elem)
+f=tkinter.Tk()
+c=tkinter.Canvas(f)
+c.config(width=500,height=500)
+c.pack()
+for elem in walah:
+    c.create_oval(elem.x-5,elem.y-5,elem.x+5,elem.y+5,fill="red")
+    c.create_text(elem.x,elem.y,text=elem.name)
+    for ng in elem.ng:
+        c.create_line(elem.x,elem.y,ng.x,ng.y)
+    
